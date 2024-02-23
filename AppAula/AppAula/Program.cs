626f0e1057df2e50de0cb704c8e39f290b0a3766
gabrielreiter <132106468+gabrielreiter@users.noreply.github.com>
2024-02-23 20:57:11 -0300

    Upload codigo resolução itens 1,2 e 4

---
+++ AppAula/AppAula/Program.cs
@@ -0,0 +1,51 @@
+﻿public class AppAula
+{
+    public static void Main()
+    {
+        //determinaBissexto();
+        //fibonaci();
+        validaTemperatura();
+    }
+
+    public static void determinaBissexto()
+    {
+        Console.WriteLine("Por gentileza, informe o ano: ");
+        var ano = Int32.Parse(Console.ReadLine());
+
+        if (DateTime.IsLeapYear(ano))
+            Console.WriteLine("Ano bissexto");
+
+    }
+
+    public static void fibonaci()
+    {
+        int numeroAnterior = 0;
+        int numeroAtual = 1;
+        int novoNumero;
+        int fibonacci;
+
+        Console.WriteLine("Informe a quantidade de vezes: ");
+        var qtt = Int32.Parse(Console.ReadLine());
+
+        for (int i = 0; i < qtt; i++)
+        {
+            fibonacci = numeroAnterior + numeroAtual;
+            Console.WriteLine(fibonacci);
+            numeroAnterior = numeroAtual;
+            numeroAtual = fibonacci;
+        }
+    }
+
+    public static void validaTemperatura()
+    {
+        Console.WriteLine("Informe a temperatura: ");
+        var temperatura = Console.ReadLine();
+
+        if (Convert.ToDecimal(temperatura) > 35)
+        {
+            Console.WriteLine("Precisa usar protetor solar");
+        }
+        else
+            Console.WriteLine("Não precisa de protetor");
+    }
+}
